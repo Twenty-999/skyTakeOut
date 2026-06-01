68dfab2dffe762fb26b8423259267bbf4e2af308
Twenty <twenty67@163.com>
2026-06-01 23:56:10 +0800

    商品浏览

--- sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.sky.entity.User;
 import com.sky.exception.LoginFailedException;
 import com.sky.mapper.UserMapper;
 import com.sky.properties.WeChatProperties;
+import com.sky.service.UserService;
 import com.sky.utils.HttpClientUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ import java.util.Map;
 
 @Service
 @Slf4j
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     // 微信服务接口地址
     public static final String WX_LOGIN_URL = "https://api.weixin.qq.com/sns/jscode2session";
