42a903dac6a71998028cd6c339cd63af9c0bc42c
Twenty <twenty67@163.com>
2026-08-13 17:11:15 +0800

    用户下单

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,21 +1,39 @@
 package com.sky.service.impl;
 
+import com.sky.constant.MessageConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.entity.AddressBook;
+import com.sky.entity.OrderDetail;
+import com.sky.entity.Orders;
+import com.sky.entity.ShoppingCart;
+import com.sky.exception.ShoppingCartBusinessException;
 import com.sky.mapper.OrderDetailMapper;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.AddressBookService;
 import com.sky.service.OrderService;
 import com.sky.vo.OrderSubmitVO;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
-
     @Autowired
     private OrderDetailMapper orderDetailMapper;
+    @Autowired
+    private AddressBookService addressBookService;
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
 
     /**
      * 用户下单
@@ -23,6 +41,50 @@ public class OrderServiceImpl implements OrderService {
      * @return
      */
     public OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO) {
-        return null;
+        AddressBook addressBook = addressBookService.getById(ordersSubmitDTO.getAddressBookId());
+        if (addressBook == null){
+            throw new RuntimeException(MessageConstant.ADDRESS_BOOK_IS_NULL);
+        }
+
+        Long useId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        shoppingCart.setUserId(useId);
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+
+        if (shoppingCartList == null || shoppingCartList.size() == 0){
+            throw new ShoppingCartBusinessException(MessageConstant.SHOPPING_CART_IS_NULL);
+        }
+
+        Orders orders = new Orders();
+        BeanUtils.copyProperties(ordersSubmitDTO, orders);
+        orders.setOrderTime(LocalDateTime.now());
+        orders.setPayMethod(Orders.UN_PAID);
+        orders.setStatus(Orders.PENDING_PAYMENT);
+        orders.setNumber(String.valueOf(System.currentTimeMillis()));
+        orders.setPhone(addressBook.getPhone());
+        orders.setConsignee(addressBook.getConsignee());
+        orders.setUserId(useId);
+
+        orderMapper.insert(orders);
+
+        List<OrderDetail> orderDetailist = new ArrayList<>();
+        for (ShoppingCart cart : shoppingCartList) {
+            OrderDetail orderDetail = new OrderDetail();
+            BeanUtils.copyProperties(cart, orderDetail);
+            orderDetail.setOrderId(orders.getId());
+            orderDetailist.add(orderDetail);
+        }
+
+        orderDetailMapper.insertBatch(orderDetailist);
+
+        shoppingCartMapper.deleteByUserId(useId);
+
+        return OrderSubmitVO.builder()
+                .id(orders.getId())
+                .orderNumber(orders.getNumber())
+                .orderAmount(orders.getAmount())
+                .orderTime(orders.getOrderTime())
+                .build();
+
     }
 }
